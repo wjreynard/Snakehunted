06f0cfdeb415e1f23b7b628fa5d4ff1f707b6bcd
Will Reynard <44241583+wjreynard@users.noreply.github.com>
2020-07-30 14:41:18 +0100

    Initial commit

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    public Transform cameraTransform;
+
+    public float speed = 6.0f;
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(inputX, 0.0f, inputY).normalized;
+
+        if (direction.magnitude >= 0.01f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# WWWWWW
+ A short platformer similar to a certain game by Terry Cavanagh, playable [here](https://wjreynard.itch.io) (soon).
+ 
+ I made this in two weeks to gain more confidence with Unity.
