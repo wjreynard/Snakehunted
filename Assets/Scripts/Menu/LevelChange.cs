a00bcd660247211023696e7155b318798f8e1dea
Will Reynard <44241583+wjreynard@users.noreply.github.com>
2020-08-13 15:01:24 +0100

    started main menu

---
+++ Assets/Scripts/Menu/LevelChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour {
+
+
+    public void StartScene() {
+        // If on last level, go to menu scene
+        // This could be improved by automatically detecting last level in build rather than manually setting it
+        if (SceneManager.GetActiveScene().buildIndex == 5)
+        {
+            SceneManager.LoadScene(0);
+        }
+        // By default, go to next scene
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
