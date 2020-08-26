33e001ff7321712f4b35f925753b4de7154d1824
Will Reynard <44241583+wjreynard@users.noreply.github.com>
2020-08-26 16:36:05 +0100

    fixed sound issue by setting pitches

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,62 +1,57 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEngine;
 using UnityEngine.Audio;
+using System;
+using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public Sound[] sounds;
 
-    // Static reference to current instance of AudioManager in scene
-    public static AudioManager instance;
+	public static AudioManager instance;
 
-    private void Awake()
-    {
-        // We only want there to be one instance of our AudioManager
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
+	public AudioMixerGroup mixerGroup;
 
-        // Let AudioManager persist between scenes
-        DontDestroyOnLoad(gameObject);
+	public Sound[] sounds;
 
-        foreach (Sound s in sounds) {
-            // add an AudioSource component and transfer attributes of the Sound to that source
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.playOnAwake = true;
-            s.source.loop = s.loop;
-            s.source.outputAudioMixerGroup = s.mixerGroup;
-        }
-    }
+	void Awake()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+
+			s.source.outputAudioMixerGroup = mixerGroup;
+		}
+	}
+
+	public void Play(string sound)
+	{
+		Debug.Log("AudioManager::Play(string)");
+
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+	}
 
     private void Start()
     {
-        PlaySound("Music");
-    }
-
-    public void PlaySound(string name)
-    {
-        Debug.Log("Playing sound: " + name);
-
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-
-        if (s == null)
-        {
-            Debug.LogWarning("Sound: " + name + " not found!");
-            return;
-        }
-
-        s.source.Play();
-    }
+		Play("Test");
+	}
 }
